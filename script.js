4aeb040546b65813817f736f94426061b58d6862
venkatesh <venkatesh.sarvapalli@gmail.com>
2017-01-25 20:55:54 +0530

    code refactoring.

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+//TODO keep all the keys and values of the expression of mathjax
 var charObj = {
-    "a2b2": '<math xmlns="http://www.w3.org/1998/Math/MathML"><mrow><msup><mi>a</mi><mn>2</mn></msup><mo>+</mo><msup><mi>b</mi><mn>2</mn></msup><mo>=</mo><msup><mi>c</mi><mn>2</mn></msup></mrow></math>',
-"a2b3":'<math xmlns="http://www.w3.org/1998/Math/MathML"><mrow><mi>A</mi><mo>=</mo><mfenced open="[" close="]"><mtable><mtr><mtd><mi>x</mi></mtd><mtd><mi>y</mi></mtd></mtr><mtr><mtd><mi>z</mi></mtd><mtd><mi>w</mi></mtd></mtr></mtable></mfenced></mrow></math>',
-"a2b4":'<math xmlns="http://www.w3.org/1998/Math/MathML"><msqrt><mn>64</mn></msqrt></math>'
-                             };
+    "a2b2": '<mrow><msup><mi>a</mi><mn>2</mn></msup><mo>+</mo><msup><mi>b</mi><mn>2</mn></msup><mo>=</mo><msup><mi>c</mi><mn>2</mn></msup></mrow>',
+    "a2b3": '<mrow><mi>A</mi><mo>=</mo><mfenced open="[" close="]"><mtable><mtr><mtd><mi>x</mi></mtd><mtd><mi>y</mi></mtd></mtr><mtr><mtd><mi>z</mi></mtd><mtd><mi>w</mi></mtd></mtr></mtable></mfenced></mrow>',
+    "a2b4": '<msqrt><mn>64</mn></msqrt>'
+};
 
 var App = {
     init: function() {
@@ -17,9 +18,9 @@ var App = {
             self.click(function() {
                 var data = self.attr("data-char");
                 if (charObj.hasOwnProperty(data)) {
-                    $("#main-text").html($("#main-text").html() + charObj[data]);
+                    $("#main-text").append(App.getMathKey(charObj[data]));
                 } else {
-                    $("#main-text").html($("#main-text").html() + data);
+                    $("#main-text").append(data);
                 }
 
             });
@@ -37,4 +38,8 @@ var App = {
             console.log("Expression : " + $("#main-text").val());
         });
     },
+
+    getMathKey : function(key){
+        return '<math xmlns="http://www.w3.org/1998/Math/MathML">'+ key + '</math>'
+    }
 }
