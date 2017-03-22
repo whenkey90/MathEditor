2c84333b1128a5a188d4e18b1bfd67aab61446fc
venkatesh <venkatesh.sarvapalli@gmail.com>
2017-03-22 09:30:15 +0530

    -- Math Editor Solution.

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ var charObj = {
     };
 	
 var keys = [];
-
+var MQ = MathQuill.getInterface(2); 
 var App = {
     init: function() {
         App.initMath();
@@ -34,48 +34,8 @@ var App = {
 			}
 		  }
 		});
-		//End
-		
-	  //var fillInTheBlank = MQ.StaticMath(document.getElementById('fill-in-the-blank'));
-	  //fillInTheBlank.innerFields[0].latex() // => 'x'
-	  //fillInTheBlank.innerFields[1].latex() // => 'y'
-	  //var mathField1 =  MQ.mathField($("#fill-in-the-blank")[0]);
-	  //mathField1.focus();
-	  var mathFieldSpan = $("#fill-in-the-blank");
-		console.log(mathFieldSpan);
-		console.log(mathFieldSpan[0]);
-		console.log(mathFieldSpan[1]);
-		var mathField = MQ.MathField(mathFieldSpan[0]);
-		console.log(mathField);
-//		mathFieldSpan.appendTo(document.body);
-		//mathField.focus();
-		var x1 = $("var mathquill-command-id");
-		// var mathField = MQ.MathField(x1);
-		// mathField.focus();
-		var parent = document.getElementsByTagName('var')[0];
-		// getElementsByAttribute('mathquill-command-id', parent);
-		console.log();
-		console.log(x1);
-		console.log(x1[0]);
-		function getElementsByAttribute(attribute, context) {
-		  var nodeList = (context || document).getElementsByTagName('*');
-		  var nodeArray = [];
-		  var iterator = 0;
-		  var node = null;
-
-		  while (node = nodeList[iterator++]) {
-			if (node.getAttribute(attribute)) nodeArray.push(node);
-		  }
-		  return nodeArray;
-		}
-		var mathFieldSpan = $(getElementsByAttribute('mathquill-command-id')[2]);
-		var mathField = MQ.MathField(mathFieldSpan[0]);
-		mathField.focus();
-		
-		//mathFieldSpan.appendTo(document.body);
-		//mathField.reflow();
-		
 	},
+
     manageKeys: function() {
         var selector = $(".char-btn");
         selector.each(function() {
@@ -84,20 +44,10 @@ var App = {
                 var $element = $("#math-field");
                 var data = self.attr("data-char");
 				keys.push(data);
-//                if (charObj.hasOwnProperty(data)) {
-//                    $("#main-text").append(App.getMathKey(charObj[data]));
-//                } else {
-//                    $("#main-text").append(data);
-//                }
-				console.log($element.text());
-				$element.text($element.text()+data);
-				//App.insertAtCaret("math-field");
-				App.initMath();
-				//$("#math-field").addClass("mq-focused");
-				//$(".mq-root-block").addClass("mq-hasCursor");
-				//$(".mq-root-block").append("<span class=\"mq-cursor\">&#8203</span>");
-
-            });
+				var mathField = MQ.MathField($element[0]);
+				mathField.cmd(data);
+				mathField.focus();
+			});
         });
 
         $("#submit-btn").click(function() {
